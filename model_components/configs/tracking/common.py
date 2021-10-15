f237851f47e5d4e7c541ee3d1df9bb5c6f9f7dd4
XuZiYi-0 <1372174278@qq.com>
2021-10-15 20:50:48 +0000

    修改gpuid

--- model_components/configs/tracking/common.py
+++ model_components/configs/tracking/common.py
@@ -3,4 +3,4 @@ from fvcore.common.config import CfgNode
 tracking_common_cfg = CfgNode()
 
 tracking_common_cfg.MODEL_NAME = 'build_deepsort'
-tracking_common_cfg.DEVICE_ID = 1,
+tracking_common_cfg.DEVICE_ID = 0,
