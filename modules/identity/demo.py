ef6b3bbeb11deec4861147d52384d0c8152b8625
XuZiYi-0 <1372174278@qq.com>
2021-10-18 09:49:14 +0000

    demo添加显示name

--- modules/identity/demo.py
+++ modules/identity/demo.py
@@ -23,9 +23,16 @@ def scale_resize(img, img_size):
     img = cv2.copyMakeBorder(img,*bound,cv2.BORDER_CONSTANT, value=(0,0,0))
     return img
 
-def demo(test_imgs, sample_path, n_p=20,n_n=10, show_img_size=(384, 192)):
+def demo(test_img_path, sample_path, n_p=20,n_n=10, show_img_size=(384, 192)):
+    test_imgs = []
+    test_img_names = os.listdir(test_img_path)[32:64]
+    for img_name in test_img_names:
+        img = cv2.imread(os.path.join(test_img_path, img_name))
+        test_imgs.append(img)
+
     personreid_img_feature_comparison = PersonReIDImg_FeatureComparison(sample_path)
     res = personreid_img_feature_comparison.run(test_imgs)
+
     print(res.keys())
     for i in range(len(test_imgs)):
         print(res['score'][i])
@@ -33,14 +40,28 @@ def demo(test_imgs, sample_path, n_p=20,n_n=10, show_img_size=(384, 192)):
         print(res['img_name'][i])
         print('#'*64)
 
+
     imgs = []
-    label = 'query'
-    t_size = cv2.getTextSize(label, 0, fontScale=0.7, thickness=1)[0]
-    for test_img in test_imgs:
+    for test_img, test_img_name in zip(test_imgs, test_img_names):
         img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
         img = scale_resize(img, show_img_size)
-        text_pos = (int(img.shape[1]/2)-int(t_size[0]/2), img.shape[0]-2-t_size[1])
-        img = cv2.putText(img, 'query', text_pos, 0, 0.7,  (255, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+
+        # 在图片上添加name
+        name = test_img_name.split('.')[0]
+        text_color = (255, 255, 255)
+        t_size_name = cv2.getTextSize(name, 0, fontScale=0.7, thickness=1)[0]
+        text_pos_label = (int(img.shape[1]/2)-int(t_size_name[0]/2), img.shape[0]-2-t_size_name[1])
+        img = cv2.putText(img, name, text_pos_label, 0, 0.7, text_color, thickness=1, lineType=cv2.LINE_AA)
+
+        # 在图片上添加‘query'
+        title = 'query'
+        text_color = (0, 255, 0)
+        t_size_title = cv2.getTextSize(title, 0, fontScale=0.7, thickness=1)[0]
+        text_pos_title = (int(img.shape[1]/2)-int(t_size_title[0]/2), img.shape[0]-6-t_size_name[1]*2)
+        img = cv2.putText(img, title, text_pos_title, 0, 0.7,  text_color, thickness=1, lineType=cv2.LINE_AA)
+
+
+
         img = np.transpose(img, (2, 0, 1))
         imgs.append(img)
     vis = Visdom()
@@ -55,17 +76,25 @@ def demo(test_imgs, sample_path, n_p=20,n_n=10, show_img_size=(384, 192)):
             rank_img = cv2.cvtColor(rank_img, cv2.COLOR_BGR2RGB)
             rank_img = scale_resize(rank_img, show_img_size)
 
-            # label = res['person_name'][i][j]+':'+res['img_name'][i][j]
-            label = '%.4f'%res['score'][i][j]
-            text_pos = (int(rank_img.shape[1]/2)-int(t_size[0]/2), rank_img.shape[0]-2-t_size[1])
-            t_size = cv2.getTextSize(label, 0, fontScale=0.7, thickness=1)[0]
+            # 在图片上添加name
+            name = str(res['img_name'][i][j]).split('.')[0]
+            text_color = (255,255,255)
+            t_size_name = cv2.getTextSize(name, 0, fontScale=0.7, thickness=1)[0]
+            text_pos_name = (int(rank_img.shape[1]/2)-int(t_size_name[0]/2), rank_img.shape[0]-2-t_size_name[1])
+            rank_img = cv2.putText(rank_img, name, text_pos_name, 0, 0.7,  text_color, thickness=1, lineType=cv2.LINE_AA)
 
+
+            # 在图片上添加相似度
+            score = '%.4f'%res['score'][i][j]
             if j < n_p:
                 text_color = (0,255,0)
             else:
                 text_color = (255,0,0)
+            t_size_score = cv2.getTextSize(score, 0, fontScale=0.7, thickness=1)[0]
+            text_pos_score = (int(rank_img.shape[1]/2)-int(t_size_score[0]/2), rank_img.shape[0]-6-t_size_name[1]*2)
+            rank_img = cv2.putText(rank_img, score, text_pos_score, 0, 0.7,  text_color, thickness=1, lineType=cv2.LINE_AA)
 
-            rank_img = cv2.putText(rank_img, label, text_pos, 0, 0.7,  text_color, thickness=1, lineType=cv2.LINE_AA)
+
 
 
 
@@ -89,4 +118,4 @@ if __name__ == '__main__':
         img = cv2.imread(os.path.join(test_img_path, img_name))
         test_imgs.append(img)
 
-    demo(test_imgs, sample_path)
+    demo(test_img_path, sample_path)
