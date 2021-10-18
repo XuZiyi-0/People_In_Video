54246492e6aa0ead8b5ffa38023923b4622df18b
XuZiYi-0 <1372174278@qq.com>
2021-10-18 09:48:17 +0000

    osnet

--- model_components/configs/person_ReID_img/common.py
+++ model_components/configs/person_ReID_img/common.py
@@ -2,5 +2,5 @@ from fvcore.common.config import CfgNode
 
 person_ReID_img_common_cfg = CfgNode()
 
-person_ReID_img_common_cfg.MODEL_NAME = 'build_mudeepv2'
-person_ReID_img_common_cfg.DEVICE_ID = 0,
+person_ReID_img_common_cfg.MODEL_NAME = 'build_osnet'
+person_ReID_img_common_cfg.DEVICE_ID = 6,
